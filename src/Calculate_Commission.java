e9e3748ee5588fe1b57da353b730b6241c5ed5fb
poonam <poonamnnaik2825@gmail.com>
2023-03-29 18:20:20 +0100

    commision program

--- src/Calculate_Commission.java
+++ src/Calculate_Commission.java
@@ -2,40 +2,48 @@ import java.util.Scanner;
 
 public class Calculate_Commission {
     Scanner scanner =  new Scanner(System.in);
+    //declaring variable
+    String sales_id;
+    String seller_name;
+
     //method with no return type and no parameters (non-static method)
     void calcCommision ()
     {
+        System.out.println("Enter the Sales id:");
+        sales_id = scanner.nextLine();
+        System.out.println("Enter the Seller's name:");
+        seller_name =scanner.nextLine();
         System.out.println("Enter the sales amount here:");
         double sales_amount = scanner.nextDouble();//scans the next token of input as double (decimal point numbers)
         if (sales_amount >= 50000.00)
         {
             System.out.println("The commission rate is 35%");
             //returns the commission amount for commission rate of 35% on sales amount
-            System.out.println("The commission amount for " + sales_amount + " is " + sales_amount * 0.35);
+            System.out.println("The commission amount for Rs." + sales_amount + " is Rs. " + sales_amount * 0.35);
         }
         else if (sales_amount>=30000.00)
         {
             System.out.println("The commission rate is 20%");
             //returns the commission amount for commission rate of 20% on sales amount
-            System.out.println("The commission amount for " + sales_amount + " is " + sales_amount * 0.20);
+            System.out.println("The commission amount for Rs." + sales_amount + " is Rs. " + sales_amount * 0.20);
         }
         else if(sales_amount>=20000.00)
         {
             System.out.println("The commission rate is 10%");
             //returns the commission amount for commission rate of 10% on sales amount
-            System.out.println("The commission amount for " + sales_amount + " is " + sales_amount * 0.10);
+            System.out.println("The commission amount for Rs." + sales_amount + " is Rs. " + sales_amount * 0.10);
         }
         else if (sales_amount>=10000.00)
         {
             System.out.println("The commission rate is 5%");
             //returns the commission amount for commission rate of 5% on sales amount
-            System.out.println("The commission amount for " + sales_amount + " is " + sales_amount * 0.5);
+            System.out.println("The commission amount for Rs." + sales_amount + " is Rs. " + sales_amount * 0.5);
         }
         else
         {
             System.out.println("The commission rate is 2%");
             //returns the commission amount for commission rate of 2% on sales amount
-            System.out.println("The commission amount for " + sales_amount + " is " + sales_amount * 0.2);
+            System.out.println("The commission amount for Rs." + sales_amount + " is Rs. " + sales_amount * 0.2);
         }
     }
 //main method
